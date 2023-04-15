c5bf617e527957b6718121ab191fde0154cd8708
shreyanshkhatri <shreyanshkhatri8@gmail.com>
2023-04-16 00:50:00 +0530

    commit3

--- src/components/News.js
+++ src/components/News.js
@@ -53,7 +53,7 @@ const News = (props)=>{
                 {loading && <Spinner />}
                 <InfiniteScroll
                 // dataLength={articles.length}
-                    dataLength={articles.length!==undefined?articles.length:0}
+                    dataLength={articles.length!=={undefined}?articles.length:0}
                     next={fetchMoreData}
                     hasMore={articles.length !== totalResults}
                     loader={<Spinner/>}
